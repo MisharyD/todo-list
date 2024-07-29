72eec29bd440ef3744f8a7e0d7c81296ef8c3b00
thord <masharyahmed123@gmail.com>
2024-07-29 10:08:01 +0300

    add structre for classes

---
+++ src/index.js
@@ -0,0 +1,19 @@
+//has name, date, priority, subtasks, and desctiption a task. can get and change all of them
+class Task {}
+
+//has name, tasks, notes, sections. can get and change all of them
+class List {}
+
+//has title, description, can get and change all of them
+class Note {}
+
+//has name, type, lists, notes. can get and change all of them
+class Goal {}
+
+//acts as a save file and as task manager, contains functions for adding, deleting tasks and lists. 
+//changing info about a task or a list. contains an array for all tasks, lists and notes. 
+class container {}
+
+//currently for command line
+class Todo {}
+
